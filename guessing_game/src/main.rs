796243614b72aa325587084972a4cb132bb865ae
jboca37 <jojadev@proton.me>
2025-02-17 15:03:56 +0000

    Finished hw assignments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+fn check_guess(guess: i32, secret: i32) -> i32 {
+    if guess < secret {
+        return -1;
+    } else if guess > secret {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = 7;
+
+    let mut guess = 10;
+
+    let mut counter = 0;
+
+    while guess != secret_number {
+        if check_guess(guess, secret_number) == -1 {
+            println!("Too low!");
+            guess += 1;
+        } else if check_guess(guess, secret_number) == 1 {
+            println!("Too high!");
+            guess -= 1;
+        }
+
+        counter += 1;
+    }
+
+    println!("You win! It took you {counter} tries.");
 }
---
+++ guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c.d
@@ -0,0 +1,5 @@
+/workspaces/rustspring2025/guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c: src/main.rs
+
+/workspaces/rustspring2025/guessing_game/target/debug/deps/guessing_game-46bb552ecb62f83c.d: src/main.rs
+
+src/main.rs:
