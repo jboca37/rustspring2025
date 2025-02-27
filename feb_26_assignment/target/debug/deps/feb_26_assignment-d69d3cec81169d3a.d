e6f2ebd8a6da65c2bf2eed7a7f91a6c5c52da8b5
jboca37 <jojadev@proton.me>
2025-02-27 05:52:24 +0000

    Attendance Verification

---
+++ feb_26_assignment/target/debug/deps/feb_26_assignment-d69d3cec81169d3a.d
@@ -0,0 +1,5 @@
+/workspaces/rustspring2025/feb_26_assignment/target/debug/deps/feb_26_assignment-d69d3cec81169d3a: src/main.rs
+
+/workspaces/rustspring2025/feb_26_assignment/target/debug/deps/feb_26_assignment-d69d3cec81169d3a.d: src/main.rs
+
+src/main.rs:
---
+++ feb_26_assignment/target/debug/feb_26_assignment.d
@@ -0,0 +1,1 @@
+/workspaces/rustspring2025/feb_26_assignment/target/debug/feb_26_assignment: /workspaces/rustspring2025/feb_26_assignment/src/main.rs
