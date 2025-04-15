5c06f3c86d5b7328f134ca50759f5cc890bebaab
jboca37 <jojadev@proton.me>
2025-04-15 03:15:10 +0000

    Closure Assignment

---
+++ closures/lazy_computation/target/debug/deps/lazy_computation-2963dda2b6f709aa.d
@@ -0,0 +1,5 @@
+/workspaces/rustspring2025/closures/lazy_computation/target/debug/deps/lazy_computation-2963dda2b6f709aa: src/main.rs
+
+/workspaces/rustspring2025/closures/lazy_computation/target/debug/deps/lazy_computation-2963dda2b6f709aa.d: src/main.rs
+
+src/main.rs:
---
+++ closures/lazy_computation/target/debug/lazy_computation.d
@@ -0,0 +1,1 @@
+/workspaces/rustspring2025/closures/lazy_computation/target/debug/lazy_computation: /workspaces/rustspring2025/closures/lazy_computation/src/main.rs
